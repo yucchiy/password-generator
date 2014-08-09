85185dc245f60e4e5ae2249ccced6899ddcf3d86
Yuichiro Mukai <y.iky917@gmail.com>
2014-08-09 13:51:57 +0900

    パスワード生成ロジックをビューに反映

--- app/scripts/popup.coffee
+++ app/scripts/popup.coffee
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Popup')
+app = new Vue {
+  el: '#password-generator',
+  methods: {
+    generate: ->
+      @user.password = base64_encode(pack('H*', md5(@user.username + "@" + @user.domain)))
+  },
+  data: {
+    user: {
+      domain: '',
+      username: '',
+      password: ''
+    }
+  },
+}
+
+if chrome.tabs
+  chrome.tabs.getSelected window.id, (tab) ->
+    app.user.domain = URI(tab.url).domain()
+
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,5 +1,27 @@
 (function() {
   'use strict';
-  console.log('\'Allo \'Allo! Popup');
+  var app;
+
+  app = new Vue({
+    el: '#password-generator',
+    methods: {
+      generate: function() {
+        return this.user.password = base64_encode(pack('H*', md5(this.user.username + "@" + this.user.domain)));
+      }
+    },
+    data: {
+      user: {
+        domain: '',
+        username: '',
+        password: ''
+      }
+    }
+  });
+
+  if (chrome.tabs) {
+    chrome.tabs.getSelected(window.id, function(tab) {
+      return app.user.domain = URI(tab.url).domain();
+    });
+  }
 
 }).call(this);
